1e26c1c4661e037cc7233d08d420f68588dfe2fb
정하 이 <ljhwsad200485@gmail.com>
2026-03-12 10:55:18 +0900

    버튼 UI 구성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightBlue;
+            button1.Font = new Font("맑은 고딕", 19.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.RoyalBlue;
+            button1.Location = new Point(715, 190);
+            button1.Name = "button1";
+            button1.Size = new Size(359, 155);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1276, 719);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
