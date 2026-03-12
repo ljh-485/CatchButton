d968da31d194618409b41f3074ce916cc48c36cd
정하 이 <ljhwsad200485@gmail.com>
2026-03-12 11:49:01 +0900

    버튼의 위치가 바뀌게 함

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,27 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            RunButton = new Button();
             SuspendLayout();
             // 
-            // button1
+            // RunButton
             // 
-            button1.BackColor = Color.LightBlue;
-            button1.Font = new Font("맑은 고딕", 19.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            button1.ForeColor = Color.RoyalBlue;
-            button1.Location = new Point(715, 190);
-            button1.Name = "button1";
-            button1.Size = new Size(359, 155);
-            button1.TabIndex = 0;
-            button1.Text = "나를 잡아봐";
-            button1.UseVisualStyleBackColor = false;
+            RunButton.BackColor = Color.LightBlue;
+            RunButton.Font = new Font("맑은 고딕", 19.875F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            RunButton.ForeColor = Color.RoyalBlue;
+            RunButton.Location = new Point(715, 190);
+            RunButton.Name = "RunButton";
+            RunButton.Size = new Size(359, 155);
+            RunButton.TabIndex = 0;
+            RunButton.Text = "나를 잡아봐";
+            RunButton.UseVisualStyleBackColor = false;
+            RunButton.MouseEnter += button1_MouseEnter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1276, 719);
-            Controls.Add(button1);
+            Controls.Add(RunButton);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -56,6 +57,6 @@
 
         #endregion
 
-        private Button button1;
+        public Button RunButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,28 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+         
+            // 1. 난수생성기준비
+            Random rd= new Random();
+
+            // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호)
+            // ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
+            int maxX= this.ClientSize.Width;int maxY= this.ClientSize.Height;
+
+            // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
+            // 버튼의 좌표 기준이 좌상단에 있기 때문에 form밖으로 잘려나오는 경우 생김 그래서 버튼의 크기만큼 빼줌
+            int nextX = rd.Next(0, maxX-RunButton.Size.Width);
+            int nextY= rd.Next(0, maxY-RunButton.Size.Height);
+
+            // 4. 위치할당(새로운Point 객체생성)
+            RunButton.Location= new Point(nextX, nextY);
+            
+            // 5. 시각적피드백(폼제목표시줄에좌표출력)
+            this.Text= $"버튼위치: ({nextX}, {nextY})";
+
+        }
     }
 }
